753d46dc431df6ee9810155eee02f4010112b740
Lee Su Hyeon <dltngus12323@gmail.com>
2023-09-21 10:39:55 +0900

    complete rectangle

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -17,7 +17,7 @@ def run_circle():
     print('CIRCLE')
 
     # 그림 그리기
-    cx, cy, r = 400, 300, 200
+    cx, cy, r = 400, 300, 250
 
     for deg in range(0, 360, 1):
         x = r * cos(radians(deg)) + cx
@@ -29,9 +29,14 @@ def run_rectangle():
     for x in range(50, 750 + 1, 10):
         render_frame(x, 90)
 
+    for y in range(90, 550, 10):
+        render_frame(750, y)
+        
     for x in range(750, 50, -10):
-        render_frame(x, 90)
+        render_frame(x, 550)
 
+    for y in range(550, 90, -10):
+        render_frame(50, y)
 
 while True:
     run_circle()
