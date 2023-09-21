54cec3debf704ea50f77b5726a3c361233924b3b
Lee Su Hyeon <dltngus12323@gmail.com>
2023-09-21 09:58:47 +0900

    make loop

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -5,4 +5,16 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_rectangle():
+    print('CIRCLE')
+    pass
+
+def run_circle():
+    print('RECTANGLE')
+    pass
+
+while True:
+    run_rectangle()
+    run_circle()
+
 close_canvas()
