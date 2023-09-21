759c1befd08cc0aca8b9a81831bfad0007aeefff
Lee Su Hyeon <dltngus12323@gmail.com>
2023-09-21 10:03:34 +0900

    draw image

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -7,6 +7,13 @@ character = load_image('character.png')
 
 def run_rectangle():
     print('CIRCLE')
+
+    # 그림 그리기
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(400, 90)
+    delay(1)
+
     pass
 
 def run_circle():
@@ -16,5 +23,6 @@ def run_circle():
 while True:
     run_rectangle()
     run_circle()
+    break
 
 close_canvas()
